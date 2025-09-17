0a8abb663bb996b7a7f61a59705add89b96007ee
Gavin Foster <gsfoster031@gmail.com>
2025-09-16 19:17:52 -0700

    Add filterByAmount function

--- Filter.hs
+++ Filter.hs
@@ -1,8 +1,12 @@
-module Filter (filterByCategory) where
+module Filter (filterByCategory, filterByMinAmount) where
 
 import Transactions (Transaction(..))
+import Text.Read (readMaybe)
 
 filterByCategory :: String -> [Transaction] -> [Transaction]
 filterByCategory category transactions
-    | null category = error "Category name cannot be empty"
-    | otherwise     = filter (\transaction -> tCategory transaction == category) transactions
+    | null category = error "Category name cannot be empty."
+    | otherwise     = filter (\ transaction -> tCategory transaction == category) transactions
+
+filterByMinAmount :: Double -> [Transaction] -> [Transaction]
+filterByMinAmount minAmount = filter (\ transaction -> tAmount transaction >= minAmount) 
