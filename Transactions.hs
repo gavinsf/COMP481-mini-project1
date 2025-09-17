891d2751361d84520f93649355f095efbfcc2766
Aziz Naji <azizneji16@gmail.com>
2025-09-16 19:05:25 -0700

    Added the format function to Transaction file

--- Transactions.hs
+++ Transactions.hs
@@ -3,6 +3,8 @@
 module Transactions (sampleTransactions, Transaction(..)) where
 
 import Data.Function (on)
+import Text.Printf (printf)
+
 
 -- Transaction type and sample data
 data Transaction = Transaction
@@ -21,3 +23,11 @@ sampleTransactions =
   , Transaction "2024-03-06" "Travel" 45.00 "Bus tickets"
   , Transaction "2024-03-07" "Food"   25.00 "Dinner with friends"
   ]
+
+-- Format results as a Table  
+formatTransactions :: [Transaction] -> String
+formatTransactions txs =
+  let header    = printf "%-12s   %-12s   %8s   %s" "Date" "Category" "Amount" "Note"
+      formatRow t = printf "%-12s | %-12s | %8.2f | %s"
+                        (tDate t) (tCategory t) (tAmount t) (tNote t)
+  in unlines (header : map formatRow txs)
