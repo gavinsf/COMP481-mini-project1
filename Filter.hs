daf8b40a3c8209a52379f4ee239e146081452c33
Gavin Foster <gsfoster031@gmail.com>
2025-09-16 21:03:14 -0700

    Add filterByYear Function

--- Filter.hs
+++ Filter.hs
@@ -1,12 +1,14 @@
-module Filter (filterByCategory, filterByMinAmount) where
+module Filter (filterByCategory, filterByMinAmount, filterByYear) where
 
 import Transactions (Transaction(..))
-import Text.Read (readMaybe)
 
 filterByCategory :: String -> [Transaction] -> [Transaction]
-filterByCategory category transactions
+filterByCategory category
     | null category = error "Category name cannot be empty."
-    | otherwise     = filter (\ transaction -> tCategory transaction == category) transactions
+    | otherwise     = filter (\ transaction -> tCategory transaction == category)
 
 filterByMinAmount :: Double -> [Transaction] -> [Transaction]
-filterByMinAmount minAmount = filter (\ transaction -> tAmount transaction >= minAmount) 
+filterByMinAmount minAmount = filter (\ transaction -> tAmount transaction >= minAmount)
+
+filterByYear :: String -> [Transaction] -> [Transaction]
+filterByYear year = filter (\ transaction -> take 4 (tDate transaction) == year) 
