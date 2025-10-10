847bfe02ea7e8bee5a567fa736593e250ac9d255
gavinsf <gsfoster031@gmail.com>
2025-10-10 12:50:16 -0700

    Add line describing output

--- Main.hs
+++ Main.hs
@@ -7,6 +7,9 @@ import Summary
 main :: IO ()
 main = do
     let output = filterByCategory "Food" sampleTransactions
+    putStrLn "Transactions by category: Food"
     putStrLn (formatTransactions output)
+
+    putStrLn "Average transaction amount by category:"
     let totals = dataByCategory average sampleTransactions
     mapM_ (\(cat, amt) -> putStrLn (cat ++ ": " ++ show amt)) totals
